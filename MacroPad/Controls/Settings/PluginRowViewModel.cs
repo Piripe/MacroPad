2a08ee41e0bd0c225b70a3dc3937127d53265d10
Piripe <77517308+Piripe@users.noreply.github.com>
2025-02-25 16:18:47 +0100

    Implemented the new plugin manager in the settings

--- MacroPad/Controls/Settings/PluginRowViewModel.cs
+++ MacroPad/Controls/Settings/PluginRowViewModel.cs
@@ -1,17 +1,30 @@
-﻿using MacroPad.Core.BasePlugin;
-using MacroPad.Shared.Plugin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Avalonia.Media.Imaging;
+using MacroPad.Core;
+using MacroPad.Core.BasePlugin;
+using MacroPad.Core.Models.Plugin;
+using MacroPad.ViewModels;
+using ReactiveUI;
+using System.IO;
 
 namespace MacroPad.Controls.Settings
 {
-    public class PluginRowViewModel
+    public class PluginRowViewModel : ViewModelBase
     {
-        public IPluginInfos Plugin {  get; set; }
-        public PluginRowViewModel(IPluginInfos plugin) { Plugin = plugin; }
-        public PluginRowViewModel() { Plugin = new BasePluginInfos(); }
+        public PluginInfos Plugin {  get; set; }
+        public PluginRowViewModel(PluginInfos plugin) { 
+            Plugin = plugin; 
+            _isEnabled = plugin.Enabled;
+            if (plugin.IconType == PluginIconType.Image) Icon = new Bitmap(Path.Combine(Plugin.PluginDirectory!, Plugin.Icon));
+        }
+        public PluginRowViewModel() { Plugin = new PluginInfos() { Name = "Dummy Plugin"}; }
+
+        private bool _isEnabled = false;
+        public bool IsEnabled { get { return _isEnabled; } set { 
+                this.RaiseAndSetIfChanged(ref _isEnabled, value); 
+                if (value) PluginManager.EnablePlugin(Plugin);
+                else PluginManager.DisablePlugin(Plugin);
+            } }
+
+        public Bitmap? Icon { get; set; }
     }
 }
